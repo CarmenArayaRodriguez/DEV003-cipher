4043dba3be273c1c5beb8cf63e46bac595a000b7
Carmen Araya Rodríguez <caarayar@uc.cl>
2022-12-02 13:06:39 -0300

    Commit primera versión funcional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
-import cipher from './cipher.js';
+window.addEventListener("load",inicio,true);
 
-console.log(cipher);
+function inicio(){
+    document.getElementById("mensajeACifrar").addEventListener("keyup", function(){
+        this.value = this.value.toUpperCase();
+    }, true);
+    
+    document.getElementById("cifrar").addEventListener("click",function(){  
+        let texto = document.getElementById("mensajeACifrar").value;
+        let desplazamiento = document.getElementById("desplazamientoCifrado").value;               
+        document.getElementById("mensajeCifrado").value = cifrar(texto, desplazamiento);
+    },true);
+
+    document.getElementById("mensajeADescifrar").addEventListener("keyup", function(){
+        this.value = this.value.toUpperCase();
+    }, true);
+    
+    document.getElementById("descifrar").addEventListener("click",function(){  
+        let texto = document.getElementById("mensajeADescifrar").value;
+        let desplazamiento = document.getElementById("desplazamientoDescifrado").value;               
+        document.getElementById("mensajeDescifrado").value = descifrar(texto, desplazamiento);
+    },true);
+}
+
+function cifrar(texto, desplazamiento) {
+    let resultado="";
+    let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    desplazamiento = desplazamiento % 26; 
+    
+    if (texto){
+        for (let i=0; i<texto.length; ++i){
+           
+            if (abecedario.indexOf(texto[i])!=-1)
+            { 
+                let posicion=((abecedario.indexOf(texto[i])+desplazamiento) % 26);
+                resultado+=abecedario[posicion];
+            }
+            else
+                resultado+=texto[i];
+        }
+    }
+    return resultado;
+}
+
+function descifrar(texto, desplazamiento) {
+    let resultado="";
+    let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    desplazamiento = desplazamiento % 26; 
+    
+    if (texto){
+        for (let i=0; i<texto.length; ++i){
+           
+            if (abecedario.indexOf(texto[i])!=-1)
+            { 
+                let posicion=((abecedario.indexOf(texto[i])-desplazamiento) % 26);
+                resultado+=abecedario[posicion];
+            }
+            else
+                resultado+=texto[i];
+        }
+    }
+    return resultado;
+}
