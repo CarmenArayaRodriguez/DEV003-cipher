c18fc581c0bf91d945373a3a47ad8bcf103a7ba8
Carmen Araya Rodríguez <caarayar@uc.cl>
2022-12-02 23:06:44 -0300

    primer test

--- src/index.js
+++ src/index.js
@@ -1,63 +1,63 @@
 window.addEventListener("load",inicio,true);
 
 function inicio(){
-    document.getElementById("mensajeACifrar").addEventListener("keyup", function(){
-        this.value = this.value.toUpperCase();
-    }, true);
+  document.getElementById("mensajeACifrar").addEventListener("keyup", function(){
+    this.value = this.value.toUpperCase();
+  }, true);
     
-    document.getElementById("cifrar").addEventListener("click",function(){  
-        let texto = document.getElementById("mensajeACifrar").value;
-        let desplazamiento = document.getElementById("desplazamientoCifrado").value;               
-        document.getElementById("mensajeCifrado").value = cifrar(texto, desplazamiento);
-    },true);
+  document.getElementById("cifrar").addEventListener("click",function(){  
+    const texto = document.getElementById("mensajeACifrar").value;
+    const desplazamiento = document.getElementById("desplazamientoCifrado").value;               
+    document.getElementById("mensajeCifrado").value = cifrar(texto, desplazamiento);
+  },true);
 
-    document.getElementById("mensajeADescifrar").addEventListener("keyup", function(){
-        this.value = this.value.toUpperCase();
-    }, true);
+  document.getElementById("mensajeADescifrar").addEventListener("keyup", function(){
+    this.value = this.value.toUpperCase();
+  }, true);
     
-    document.getElementById("descifrar").addEventListener("click",function(){  
-        let texto = document.getElementById("mensajeADescifrar").value;
-        let desplazamiento = document.getElementById("desplazamientoDescifrado").value;               
-        document.getElementById("mensajeDescifrado").value = descifrar(texto, desplazamiento);
-    },true);
+  document.getElementById("descifrar").addEventListener("click",function(){  
+    const texto = document.getElementById("mensajeADescifrar").value;
+    const desplazamiento = document.getElementById("desplazamientoDescifrado").value;               
+    document.getElementById("mensajeDescifrado").value = descifrar(texto, desplazamiento);
+  },true);
 }
 
 function cifrar(texto, desplazamiento) {
-    let resultado="";
-    let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    desplazamiento = desplazamiento % 26; 
+  let resultado="";
+  const abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  desplazamiento = desplazamiento % 26; 
     
-    if (texto){
-        for (let i=0; i<texto.length; ++i){
+  if (texto){
+    for (let i=0; i<texto.length; ++i){
            
-            if (abecedario.indexOf(texto[i])!=-1)
-            { 
-                let posicion=((abecedario.indexOf(texto[i])+desplazamiento) % 26);
-                resultado+=abecedario[posicion];
-            }
-            else
-                resultado+=texto[i];
-        }
+      if (abecedario.indexOf(texto[i])!==-1)
+      { 
+        const posicion=((abecedario.indexOf(texto[i])+desplazamiento) % 26);
+        resultado+=abecedario[posicion];
+      }
+      else
+        resultado+=texto[i];
     }
-    return resultado;
+  }
+  return resultado;
 }
 
 function descifrar(texto, desplazamiento) {
-    let resultado="";
-    let abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    desplazamiento = desplazamiento % 26; 
+  let resultado="";
+  const abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  desplazamiento = desplazamiento % 26; 
     
-    if (texto){
-        for (let i=0; i<texto.length; ++i){
+  if (texto){
+    for (let i=0; i<texto.length; ++i){
            
-            if (abecedario.indexOf(texto[i])!=-1)
-            { 
-                let posicion=((abecedario.indexOf(texto[i])-desplazamiento) % 26);
-                resultado+=abecedario[posicion];
-            }
-            else
-                resultado+=texto[i];
-        }
+      if (abecedario.indexOf(texto[i])!==-1)
+      { 
+        const posicion=((abecedario.indexOf(texto[i])-desplazamiento) % 26);
+        resultado+=abecedario[posicion];
+      }
+      else
+        resultado+=texto[i];
     }
-    return resultado;
+  }
+  return resultado;
 }
