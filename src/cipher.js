6d71e5174a7c24322d91273452718a6d427acd7a
Carmen Araya Rodríguez <caarayar@uc.cl>
2022-12-06 10:19:44 -0300

    proyecto con módulos funcional

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,45 @@
-var document= document.getElementById("mensajeACifrar").addEventListener("keyup", function(){
-    this.value = this.value.toUpperCase();
-  });
-
-  var document= document.getElementById("cifrar").addEventListener("click",function(){  
-    const texto = document.getElementById("mensajeACifrar").value;
-     const desplazamiento = document.getElementById("desplazamientoCifrado").value;               
-  document.getElementById("mensajeCifrado").value = cifrar(texto, desplazamiento);
-  });
-
-  function cifrar(texto, desplazamiento) {
-    let resultado="";
+const cipher = {
+  code: (texto, desplazamiento) => {
+    let resultado = "";
     const abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    desplazamiento = desplazamiento % 26; 
-      
-    if (texto){
-      for (let i=0; i<texto.length; ++i){
-             
-        if (abecedario.indexOf(texto[i])!==-1)
-        { 
-          const posicion=((abecedario.indexOf(texto[i])+desplazamiento) % 26);
-          resultado+=abecedario[posicion];
+    desplazamiento = desplazamiento % 26;
+
+    if (texto) {
+      for (let i = 0; i < texto.length; ++i) {
+
+        if (abecedario.indexOf(texto[i]) !== -1) {
+          const posicion = ((abecedario.indexOf(texto[i]) + desplazamiento) % 26);
+          resultado += abecedario[posicion];
         }
         else
-          resultado+=texto[i];
+          resultado += texto[i];
       }
     }
     return resultado;
+  },
+
+
+  decode: (texto, desplazamiento) => {
+    let resultado = "";
+    const abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    desplazamiento = desplazamiento % 26;
+
+    if (texto) {
+      for (let i = 0; i < texto.length; ++i) {
+
+        if (abecedario.indexOf(texto[i]) !== -1) {
+          const posicion = ((abecedario.indexOf(texto[i]) - desplazamiento) % 26);
+          resultado += abecedario[posicion];
+        }
+        else {
+          resultado += texto[i];
+        }
+
+      }
+      return resultado;
+    }
+
   }
+}
+
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
+import cipher from "./cipher.js";
 
+let messageToUpperCase= document.getElementById("mensajeACifrar");
+messageToUpperCase.addEventListener("keyup", function(){
+    this.value = this.value.toUpperCase();
+  });
+
+  let btnCipher= document.getElementById("cifrar");
+  btnCipher.addEventListener("click",function(){  
+    const texto = document.getElementById("mensajeACifrar").value;
+     const desplazamiento = document.getElementById("desplazamientoCifrado").value;               
+  document.getElementById("mensajeCifrado").value = cipher.code(texto, desplazamiento);
+  });
+
+  let messageToUpperCaseDecode= document.getElementById("mensajeADescifrar");
+  messageToUpperCaseDecode.addEventListener("keyup", function(){
+    this.value = this.value.toUpperCase();
+  });
+    
+  let btnDecipher= document.getElementById("descifrar");
+  btnDecipher.addEventListener("click",function (){  
+    const texto = document.getElementById("mensajeADescifrar").value;
+    const desplazamiento = document.getElementById("desplazamientoDescifrado").value;               
+    document.getElementById("mensajeDescifrado").value = cipher.decode(texto, desplazamiento);
+  });
+  
+console.log(cipher);
  
