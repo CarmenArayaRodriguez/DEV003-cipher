763b0e6205059842299b55aa215cd9bfc19150a8
Carmen Araya Rodríguez <caarayar@uc.cl>
2022-12-07 22:36:32 -0300

    cifrador descifrador en un solo panel funcional sin css

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 import cipher from "./cipher.js";
 
-let messageToUpperCase= document.getElementById("mensajeACifrar");
+const messageToUpperCase= document.getElementById("mensajeACifrarDescifrar");
 messageToUpperCase.addEventListener("keyup", function(){
-    this.value = this.value.toUpperCase();
-  });
+  this.value = this.value.toUpperCase();
+});
 
-  let btnCipher= document.getElementById("cifrar");
-  btnCipher.addEventListener("click",function(){  
-    const texto = document.getElementById("mensajeACifrar").value;
-     const desplazamiento = document.getElementById("desplazamientoCifrado").value;               
-  document.getElementById("mensajeCifrado").value = cipher.code(texto, desplazamiento);
-  });
+const btnCipher= document.getElementById("cifrar");
+btnCipher.addEventListener("click",function(){  
+  const texto = document.getElementById("mensajeACifrarDescifrar").value;
+  const desplazamiento = document.getElementById("desplazamientoCifradoDescifrado").value;               
+  document.getElementById("mensajeCifradoDescifrado").value = cipher.code(texto, desplazamiento);
+});
 
-  let messageToUpperCaseDecode= document.getElementById("mensajeADescifrar");
-  messageToUpperCaseDecode.addEventListener("keyup", function(){
-    this.value = this.value.toUpperCase();
-  });
-    
-  let btnDecipher= document.getElementById("descifrar");
-  btnDecipher.addEventListener("click",function (){  
-    const texto = document.getElementById("mensajeADescifrar").value;
-    const desplazamiento = document.getElementById("desplazamientoDescifrado").value;               
-    document.getElementById("mensajeDescifrado").value = cipher.decode(texto, desplazamiento);
-  });
+const btnDecipher= document.getElementById("descifrar");
+btnDecipher.addEventListener("click",function (){  
+  const texto = document.getElementById("mensajeACifrarDescifrar").value;
+  const desplazamiento = document.getElementById("desplazamientoCifradoDescifrado").value;               
+  document.getElementById("mensajeCifradoDescifrado").value = cipher.decode(texto, desplazamiento);
+});
   
 console.log(cipher);
  
